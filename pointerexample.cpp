bc336a68544bbfb9d0f291c5f46dd378c5d2bf7c
kimdongyeob <kimdongyeob@snu.ac.kr>
2020-06-28 01:17:50 +0000

    merge C container

---
+++ pointerexample.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+int main()
+{
+	int firstvalue, secondvalue;
+	int *mypointer;
+	mypointer =&firstvalue;
+	*mypointer=10;
+	mypointer = &secondvalue;
+	*mypointer=20;
+	std::cout << "firstvalue is " << firstvalue << '\n';
+	std::cout << "secondvalue is " << secondvalue << '\n';
+	return 0;
+}
